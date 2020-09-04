e553aa0b9ff1adb1bad78fea49c7d8dfedec5bc4
srvarma7 <srvarma7@gmail.com>
2020-09-04 11:38:23 +1000

    Adding .mlmodel to gitignore file

--- ImageClassificatiion/ImageClassificatiion/Manager/ImageDetectionManager.swift
+++ ImageClassificatiion/ImageClassificatiion/Manager/ImageDetectionManager.swift
@@ -9,4 +9,8 @@ import Foundation
 
 class ImageDetectionManager {
     
+    /*
+     Resnet50.mlmodel is not uploaded to the Repo
+     Please go to https://developer.apple.com/machine-learning/models/ and download the mlmodel
+     */
 }
