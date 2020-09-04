d2155c6607c81e1697cb4ce8375cc7b525f18508
srvarma7 <srvarma7@gmail.com>
2020-09-04 12:07:25 +1000

    Implementing ImageDetectionManager

--- ImageClassificatiion/ImageClassificatiion/Manager/ImageDetectionManager.swift
+++ ImageClassificatiion/ImageClassificatiion/Manager/ImageDetectionManager.swift
@@ -6,6 +6,8 @@
 //
 
 import Foundation
+import CoreML
+import UIKit
 
 class ImageDetectionManager {
     
@@ -13,4 +15,21 @@ class ImageDetectionManager {
      Resnet50.mlmodel is not uploaded to the Repo
      Please go to https://developer.apple.com/machine-learning/models/ and download the mlmodel
      */
+    
+    let model: Resnet50
+    
+    init() {
+        model = Resnet50()
+    }
+    
+    func classifyImage(img: UIImage) -> String? {
+        
+        guard let pixelBuffer = img.toCVPixelBuffer(),
+              let predection = try? model.prediction(image: pixelBuffer)
+              else {
+            return nil
+        }
+        
+        return predection.classLabel
+    }
 }
